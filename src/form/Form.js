654b2d3ce5cf48ad0c2a3acf58dc5fca811b35eb
EstevamPonte <tecoponte@gmail.com>
2019-06-01 17:27:42 -0300

    estudando o json,  fazendo requisições http, estudando como as coisas funcionam

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,90 +1,87 @@
-import React, { Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import MyCard from '../card/MyCard'
-import jQuery from 'jquery'
+import axios from 'axios'
 
 class Form extends Component {
-    componentDidMount(){
-        this._serchInfo()
-        this._Info(this._addInfo)
-    }
 
-    _serchInfo(){
-        jQuery.ajax({
-            method: 'GET',
-            url: 'http://localhost:3004/importants',
-            success: (importants) => {
-                this.setState({importants: importants})
-            },
-        })
-    }
+   constructor(props) {
+      super(props)
+      this.state = {
+         infoCarros: [],
+         title: "",
+         description: ""
+      }
+   }
 
-    _Info(){
-        jQuery.ajax({
-            method: 'POST',
-            url: 'http://localhost:3004/importants',
-            data: this._addInfo,
-            success: function(data) { alert('data: ' + data); },
-            contentType: "application/json",
-            dataType: 'json'
-        })
- }
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            importants: []
-        }
-        
-    }
-
-    _addInfo(title, description){
-    const important = {
-        title,
-        description,
-
-        id: this.state.importants.length + 1
-    }
-    this.setState({
-        importants: this.state.importants.concat([important])
-    })
-}
-
-    _getInfo() {
-        
-        return this.state.importants.map( (important) => <MyCard 
-            title={important.title} description={important.description} 
-            key={important.id}/>)       
-    }
-
-    _hendleSubmit(event) {
-        event.preventDefault();
-        let title = this._title.value
-        let description = this._description.value
-
-        this._addInfo(title, description)
-    }
-
+   componentDidMount() {
+      this._serchInfo()
+   }
    
-    
-    render() {
-        const important = this._getInfo()
-        return (
-            <Fragment>
-                <form>
-                    <div className="input-group mb-3" onSubmit={this._hendleSubmit.bind(this)}>
-                        <div className="input-group-prepend">
-                            <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={this._hendleSubmit.bind(this)}>Botão</button>
-                        </div>
-                        <div>
-                            <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" ref={ input => this._title = input}/>
-                            <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" ref={ input => this._description = input}/>
-                        </div>
-                    </div>
-                </form>
-                {important}
-            </Fragment>
-        );
-    }
+   _serchInfo() {
+      axios.get('http://localhost:3000/importants')
+         .then(resp => {
+            this.setState({infoCarros: resp.data})
+         })
+         .catch(error => console.log('errouu'))
+   }
+
+   _postInfo(carro) {
+      axios.post('http://localhost:3000/importants', carro)
+         .then(resp => console.log(carro))
+         .catch(error => console.log (error))
+   }
+
+   addInfoCard() {
+      return this.state.infoCarros.map((carro) => <MyCard
+         title={carro.title} description={carro.description}
+         key={carro.id} />)
+   }
+
+   _handleChange(event){
+      event.preventDefault()
+      this.setState({
+         [event.target.name]: event.target.value
+      })
+   }
+
+   _hendleSubmit(event) {
+      event.preventDefault();
+      let carro = {
+         title: this.state.title,
+         description: this.state.description,
+      }
+      this._postInfo(carro)
+      this._serchInfo()
+   }
+
+   render() {
+      const infoCard = this.addInfoCard()
+      const { title, description } = this.state
+      return (
+         <Fragment>
+            <form onSubmit={this._hendleSubmit.bind(this)}>
+               <div className="input-group mb-3" >
+                  <div className="input-group-prepend">
+                     <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={this._hendleSubmit.bind(this)}>Botão</button>
+                  </div>
+                  <div>
+                     <input type="text" key="title" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"
+                        name="title"
+                        onChange={this._handleChange.bind(this)}
+                        value={title}
+                      />
+                     <input type="text" key="description" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"
+                        name="description"
+                        onChange={this._handleChange.bind(this)}
+                        values={description}
+                      />
+                  </div>
+               </div>
+            </form>
+            {infoCard}
+         </Fragment>
+      );
+   }
 }
 
 export default Form;
